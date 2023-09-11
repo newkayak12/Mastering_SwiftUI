566f1f8fd51d45898f237e1e017e3dce405ff494
newkayak12 <newkayak12@gmail.com>
2023-09-11 22:36:57 +0900

    swiftUI

--- Navigation/Main/MainView.swift
+++ Navigation/Main/MainView.swift
@@ -1,31 +1,83 @@
-//
-//  Mastering SwiftUI
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import SwiftUI
 
 struct MainView: View {
+    @State private var isHidden = false
+    @State private var push = false
+    
     var body: some View {
-        Text("Hello, World!")        
+        NavigationView {
+            VStack {
+                Text("Hello, World!")
+                
+                Button {
+                    isHidden.toggle()
+                } label: {
+                    Text("toggle")
+                }
+                
+                
+                Button {
+                    push = true
+                } label: {
+                    Text("conditionalPush")
+                }
+                .padding()
+                
+                NavigationLink("buttonPush", isActive: $push) {
+                    EmojiView(emoji: "😃")
+                        .navigationTitle("SMILE")
+                }.hidden()
+                //envVariable 또는 push를 보내서 false로 변경하면 pop 된다.
+                
+                NavigationLink("Push") {
+                    EmojiView(emoji: "😃")
+                        .navigationTitle("SMILE")
+                }
+                .navigationTitle("Hello")
+                         
+                         
+            }
+            .navigationTitle("Navigation") //반드시 embed된 최상단의 view에 추가해야 한다.
+//            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(isHidden)
+            .toolbar {
+//                ToolbarItemGroup(placement: .navigationBarLeading) {
+//                    HStack {
+//                        Button {
+//                            isHidden.toggle()
+//                        } label: {
+//                            Text("toggle")
+//                        }
+//
+//                        Button {
+//                            isHidden.toggle()
+//                        } label: {
+//                            Text("toggle")
+//                        }
+//                    }
+//                }
+//
+//
+//
+//                ToolbarItemGroup(placement: .navigationBarTrailing) {
+//                    HStack {
+//                        Button {
+//                            isHidden.toggle()
+//                        } label: {
+//                            Text("toggle")
+//                        }
+//
+//                        Button {
+//                            isHidden.toggle()
+//                        } label: {
+//                            Text("toggle")
+//                        }
+//                    }
+//                }
+                
+            }
+            
+        }
     }
 }
 
--- TabNavigation/Tab/Tab.swift
+++ TabNavigation/Tab/Tab.swift
@@ -1,35 +1,32 @@
-//
-//  Mastering SwiftUI
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import SwiftUI
 
 struct Tab_Tutorials: View {
+    @State private var selectedIndex = 0
+    
     var body: some View {
-        VStack {
-            SymbolScene(name: "star", color: Color.red)
-            SymbolScene(name: "heart", color: Color.green)
-            SymbolScene(name: "play", color: Color.blue)
+        TabView(selection: $selectedIndex) {
+            SymbolScene(name: "star", color: Color.red, selectedIndex: $selectedIndex)
+                .tabItem {
+                    Label("star", systemImage: "star")
+                }
+                .tag(0)
+            
+            SymbolScene(name: "heart", color: Color.green, selectedIndex: $selectedIndex)
+                .tabItem {
+                    Label("heart", systemImage: "heart")
+                }
+                .tag(1)
+            
+            SymbolScene(name: "play", color: Color.blue, selectedIndex: $selectedIndex)
+                .tabItem {
+                    Label("play", systemImage: "play")
+                }
+                .tag(2)
         }
+        .onChange(of: selectedIndex) { newValue in
+            print(newValue)
+        }
+        
     }
 }
 
