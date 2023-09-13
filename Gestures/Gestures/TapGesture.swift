d2645cd1e4ed6cfe366324e33370ce6ac33680ee
newkayak12 <newkayak12@gmail.com>
2023-09-13 23:33:35 +0900

    swiftUI

--- Gestures/Gestures/TapGesture.swift
+++ Gestures/Gestures/TapGesture.swift
@@ -1,30 +1,20 @@
-//
-//  Mastering SwiftUI
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import SwiftUI
 
 struct TapGesture_Tutorials: View {
     @State private var tapCount = 0
+    var tapToPlus: some Gesture {
+        TapGesture()
+            .onEnded{
+                tapCount += 1
+            }
+    }
+    
+    var doubleTapToPlus10: some Gesture {
+        TapGesture(count: 2)
+            .onEnded{
+                tapCount += 10
+            }
+    }
     
     var body: some View {
         VStack {
@@ -36,11 +26,22 @@ struct TapGesture_Tutorials: View {
                     .font(.system(size: 100))
                     .foregroundColor(.red)
                     .padding()
+//                    .onTapGesture(count: 2) { //더블탭
+//                        tapCount -= 1
+//                    }
+                    .gesture(TapGesture().onEnded({
+                        tapCount -= 1
+                    }))
                 
                 Image(systemName: "plus.circle")
                     .font(.system(size: 100))
                     .foregroundColor(.blue)
                     .padding()
+                    .gesture(doubleTapToPlus10) //순서가 중요하다.
+                    .gesture(tapToPlus)
+//                    .onTapGesture {
+//                        tapCount += 1
+//                    }
             }
         }
     }
