e457d3130ad4fb111dc9a159862bc8dd491c27a1
newkayak12 <newkayak12@gmail.com>
2023-09-15 00:03:38 +0900

    swiftUI

--- Gestures/Gestures/DragGesture.swift
+++ Gestures/Gestures/DragGesture.swift
@@ -1,34 +1,51 @@
-//
-//  Mastering SwiftUI
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import SwiftUI
 
-struct DragGesture_Tutorials: View {    
+struct DragGesture_Tutorials: View {
+    @State private var currentTranslation = CGSize.zero
+    @State private var finalTranslation = CGSize.zero
+    
+    @GestureState private var curT = CGSize.zero //읽기 전용이 되며, 초기화할 필요도 없어진다.
+    
+    var dragGesture: some Gesture {
+//        .local 기본
+//        .global 전역
+        DragGesture()
+//            .onChanged { value in
+//                print("START \(value)")
+//
+//                currentTranslation = value.translation
+//            }
+//            .onEnded { value in
+//                print("END \(value)")
+//                currentTranslation = .zero
+//                var t = finalTranslation
+//                t.width += value.translation.width
+//                t.height += value.translation.height
+//                finalTranslation = t
+//            }
+        
+            .updating($curT, body: { value, state, transaction in
+                state = value.translation
+                
+            })
+            .onEnded { value in
+                var t = finalTranslation
+                t.width += value.translation.width
+                t.height += value.translation.height
+                finalTranslation = t
+            }
+    }
+    
     var body: some View {
         VStack {
             Circle()
                 .foregroundColor(.yellow)
-                .frame(width: 100, height: 100)            
+                .frame(width: 100, height: 100)
+//                .onDrag(<#T##data: () -> NSItemProvider##() -> NSItemProvider#>)//DragOnDrop에 사용
+                .offset(finalTranslation)
+                .offset(currentTranslation)
+                .gesture(dragGesture)
+            
         }
     }
 }
